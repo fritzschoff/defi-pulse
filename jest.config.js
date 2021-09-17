794cd4b704e02610b1348452ff3661fcd7b95e7e
Max Fritz <39312833+fritzschoff@users.noreply.github.com>
2021-09-17 17:18:18 +0200

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -9,7 +9,7 @@ module.exports = {
     "^.+\\.(css|sass|scss)$": "<rootDir>/__mocks__/styleMock.js",
     "^.+\\.(jpg|jpeg|png|gif|webp|svg)$": "<rootDir>/__mocks__/fileMock.js",
     "@components/(.*)": "<rootDir>/components/$1",
-    "@interfaces/(.*)": "<rootDir>/components/$1",
+    "@interfaces/(.*)": "<rootDir>/interfaces/$1",
   },
   testPathIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/.next/"],
   testEnvironment: "jsdom",
